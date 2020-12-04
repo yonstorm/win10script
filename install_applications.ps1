04d8cdb125c242b5f5533fa962e448addbe0a1a0
Joni Strömberg <joni.stromberg@gmail.com>
2020-12-04 08:02:27 -0800

    Update install_applications.ps1

--- install_applications.ps1
+++ install_applications.ps1
@@ -1,5 +1,5 @@
-$apps = @('Microsoft.PowerToys','elgato.streamdeck','Spotify.Spotify','Git.Git','JetBrains.Toolbox','Alacritty.Alacritty','Discord.Discord','7zip.7zip','VideoLAN.VLC','Valve.Steam','UnityTechnologies.UnityHub','BraveSoftware.BraveBrowser','LogMeIn.LastPass','vim.vim')
-
+$apps = @('Microsoft.PowerToys','elgato.streamdeck','Git.Git','JetBrains.Toolbox','Alacritty.Alacritty','Discord.Discord','7zip.7zip','VideoLAN.VLC','Valve.Steam','UnityTechnologies.UnityHub','BraveSoftware.BraveBrowser','LogMeIn.LastPass','vim.vim')
+#'Spotify.Spotify', wont install with admin privileges, because of reasons
 Foreach ($app in $apps)
 {
     winget install $app --silent
