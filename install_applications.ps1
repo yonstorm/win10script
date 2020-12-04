70e926f119dd2bf831e744b67cae842a42460789
Joni Strömberg <joni.stromberg@gmail.com>
2020-12-04 08:25:41 -0800

    Update install_applications.ps1

--- install_applications.ps1
+++ install_applications.ps1
@@ -1,4 +1,4 @@
-$apps = @('Microsoft.PowerToys','elgato.streamdeck','Git.Git','JetBrains.Toolbox','Alacritty.Alacritty','Discord.Discord','7zip.7zip','VideoLAN.VLC','Valve.Steam','UnityTechnologies.UnityHub','BraveSoftware.BraveBrowser','LogMeIn.LastPass','vim.vim')
+$apps = @('Microsoft.VC++2015-2019Redist-x86','Microsoft.VC++2015-2019Redist-x64','Microsoft.PowerToys','elgato.streamdeck','Git.Git','JetBrains.Toolbox','Alacritty.Alacritty','Discord.Discord','7zip.7zip','VideoLAN.VLC','Valve.Steam','UnityTechnologies.UnityHub','BraveSoftware.BraveBrowser','LogMeIn.LastPass','vim.vim')
 #'Spotify.Spotify', wont install with admin privileges, because of reasons
 Foreach ($app in $apps)
 {
