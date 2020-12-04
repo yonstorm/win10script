b38240f61594c5dbeb656fa9f2805dbffb3b2aa0
Joni Strömberg <joni.stromberg@gmail.com>
2020-12-04 03:49:01 -0800

    Update minimal.ps1

--- minimal.ps1
+++ minimal.ps1
@@ -8,7 +8,7 @@
 #
 #	Addition: One command to rule them all, One command to find it, and One command to Run it! 
 #
-#     > powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://git.io/JJ8R4')"
+#     > powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/yonstorm/win10script/master/minimal.ps1')"
 #
 #	Chris Titus Tech Additions:
 #
